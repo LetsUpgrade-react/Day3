f4cb3d620b5f308738f30d1d129e4c6ad31f26a4
HariharanRaveendar <harideveloper01@gmail.com>
2021-07-16 13:40:37 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import './style.css';
+import Game from './Game.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Main =()=>{
+    return (
+        <div>
+            <h1 style={{textAlign:'center',color:'white'}}>
+            Lets Upgrade Find Winner Logic Application Assingment
+            </h1>
+            <p style={{textAlign:'center',color:'white',fontFamily:'Lucida Console'}}>
+                Welcome to React Js Development Program!.
+            </p>
+            <h3 style={{textAlign:'center',color:'white',fontFamily:'Lucida Console'}}>Day 3 Assignment</h3>
+            <div style={{
+                          display: "flex",
+                          justifyContent: "center",
+                          alignItems: "center"
+            }}>
+                <Game></Game>
+                </div>
+        </div>
+
+    )
 }
-
-export default App;
+export default Main
